30e9a26f3a14a0fc74d306c5bb3184e8a1044b78
yeah1tsmark <marcwafula635@gmail.com>
2023-04-13 10:39:05 +0300

    first commit

---
+++ app/src/main/java/com/example/simplelistviewapp_mark/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.simplelistviewapp_mark
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}
